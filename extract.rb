025735db7f99ce8c5965075ec3b8050f72435107
Yuka Kato <satoimo.to.imo@gmail.com>
2017-02-24 00:45:22 +0900

    It works

--- extract.rb
+++ extract.rb
@@ -14,7 +14,12 @@ xlsx = Roo::Excelx.new("./yochiyochirb-tickets-57728.xlsx")
 # NOTE: やりたいことカラムの、有効な値があるセルを抜き出す。[0] は header なので無視してる。
 todos = xlsx.sheet("参加者").column(7)[1..-1].select{|todo| !todo.empty? }
 
-Octokit.configure do |c|
-  c.login = ENV["GITHUB_USERNAME"]
-  c.password = ENV["GITHUB_PASSWORD"]
+client = Octokit::Client.new(access_token: ENV["GITHUB_TOKEN"])
+
+project = client.projects(ENV["REPO_NAME"]).select{|proj| proj[:name] == ENV["PROJECT_NAME"]}.shift
+
+column = client.project_columns(project[:id]).select{|col| col[:name] == ENV["COLUMN_NAME"]}.shift
+
+todos.each do |todo|
+  client.create_project_card(column[:id], note: todo)
 end
