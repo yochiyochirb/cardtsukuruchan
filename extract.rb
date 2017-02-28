a99a36af130b9daf871d3c6d014d65319a418622
Yuka Kato <satoimo.to.imo@gmail.com>
2017-02-28 22:38:17 +0900

    Tidy

--- extract.rb
+++ extract.rb
@@ -15,7 +15,6 @@ todos = xlsx.sheet("参加者").column(7)[1..-1].reject{|todo| todo.nil? || todo
 
 client = Octokit::Client.new(access_token: ENV["GITHUB_TOKEN"])
 project = client.projects(ENV["REPO_NAME"], accept: MEDIA_TYPE).find{|proj| proj[:name] == ENV["PROJECT_NAME"] }
-
 column = client.project_columns(project[:id], accept: MEDIA_TYPE).find{|col| col[:name] == ENV["COLUMN_NAME"] }
 
 todos.each do |todo|
